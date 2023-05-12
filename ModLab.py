f78f5032cc0cf80ce2c62e07bb77578c119b0925
Nick Persing <nicholaspersing@gmail.com>
2023-05-12 17:38:15 -0400

    Added logic for quirks merging

--- ModLab.py
+++ ModLab.py
@@ -1,9 +1,28 @@
 import os
 import sys
 import json
+import argparse
 
 DEFAULT_MOD_FOLDER = "mods"
 DEFAULT_OUTPUT_FOLDER = "output"
+
+
+# Create the parser
+parser = argparse.ArgumentParser(description="Merge JSON files.")
+
+# Add optional arguments for the source and output directories
+# If the user does not provide these arguments, default values are used
+parser.add_argument('-s', '--source', type=str, default=DEFAULT_MOD_FOLDER,
+                    help="The mods' source directory containing the JSON files to merge.")
+parser.add_argument('-o', '--output', type=str, default=DEFAULT_OUTPUT_FOLDER,
+                    help="The directory where the merged JSON file will be saved.")
+
+# Parse the command line arguments
+args = parser.parse_args()
+
+# Now you can access the source and output directories like so:
+mod_folder = args.source
+output_directory = args.output
 
 # Check if the user specified a file path to the mod folders
 if len(sys.argv) > 1:
@@ -12,14 +31,18 @@ else:
     mod_folder = DEFAULT_MOD_FOLDER
 
 print(f"Using mod folder: {mod_folder}")
+print(f"Using output folder: {output_directory}")
 
-# scan the subfolders of the mod folder for the mechs.json files
+# scan the subfolders of the mod folder for the mechs.json and quirk.json files
 mech_files = []
+quirk_files = []
 
 for root, dirs, files in os.walk(mod_folder):
     for file in files:
         if file.endswith("mechs.json"):
             mech_files.append(os.path.join(root, file))
+        elif file.endswith("quirks.json"):
+            quirk_files.append(os.path.join(root, file))
 
 print(f"Found {len(mech_files)} mech files.")
 
@@ -98,5 +121,25 @@ if not os.path.exists(DEFAULT_OUTPUT_FOLDER):
 # write the combined mechs to a new file
 with open(f"{DEFAULT_OUTPUT_FOLDER}/mechs.json", "w") as f:
     json.dump(mechs, f, indent=2)
+
+
+# read the first quirk file into the dictionary
+with open(quirk_files[0]) as f:
+    quirks = json.load(f)
+
+# remove the first quirk file from the list
+quirk_files.pop(0)
+
+# read and merge the remaining quirk files into the dictionary
+for quirk_file in quirk_files:
+    with open(quirk_file) as f:
+        quirk_data = json.load(f)
+        quirks = join_dicts(quirks, quirk_data)
+
+print(f"Loaded {len(quirks)} quirks.")
+
+# write the combined quirks to a new file
+with open(f"{DEFAULT_OUTPUT_FOLDER}/quirks.json", "w") as f:
+    json.dump(quirks, f, indent=2)
 
 print("Done.")
